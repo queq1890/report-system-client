49ab9c6c045282a9721c06dd84b5f46dd78aad6a
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-20 10:50:22 +0700

    Define Router

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import AppContainer from 'containers/AppContainer';
 import store from './store';
 import 'semantic-ui-css/semantic.min.css';
 import './main.css';
 
+const history = createBrowserHistory();
+
 render(
   <Provider store={store}>
-    <AppContainer />
+    <Router history={history}>
+      <AppContainer />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
