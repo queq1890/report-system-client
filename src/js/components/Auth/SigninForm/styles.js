e308218c03a903091bdd269da19601765a3adac9
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-24 08:25:43 +0700

    delete unused Img component

--- src/js/components/Auth/SigninForm/styles.js
+++ src/js/components/Auth/SigninForm/styles.js
@@ -1,14 +1,8 @@
 import styled from 'styled-components';
 
-const Img = styled.img`
-  height: 40px;
-  display: -webkit-inline-box;
-  position: relative;
-`;
-
 const Container = styled.div`
   margin: 0 auto;
   width: 400px;
 `;
 
-export { Img, Container };
+export { Container };
